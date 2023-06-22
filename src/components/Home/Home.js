b4b3f4a9c674be3f413721967d0973e53953e696
Gokada-Prasanna <prasannagokada1996@gmail.com>
2023-06-22 16:06:13 +0530

    ok

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,17 +1,19 @@
 import React from 'react'
 import {useNavigate} from 'react-router-dom'
+import './index.css'
+
 function Home() {
   const navigate=useNavigate()
   return (
     <>
-        <div style={{backgroundColor:'#1d1a69',color:'white',height:'60px',display:'flex',flexDirection:'row',justifyContent:'space-between',alignItems:'center', paddingRight:'15px', paddingLeft:'15px'}}>
-              <div style={{display:'flex',alignItems:'center'}}>
+        <div className="headerContainer">
+              <div className="headerLogoContainer">
               <img src="https://res.cloudinary.com/dufx8zalt/image/upload/v1687419355/logoimage1_krvkbq.png" alt="logo" style={{height:'50px', width:'100px', borderRadius:'10px'}}/>
               </div>
-              <div style={{display:'flex',flexDirection:'row',alignItems:'center'}}>
-              <p onClick={()=>navigate('/')} style={{fontSize:'18px',marginRight:'20px'}}>Home</p>
-              <p onClick={()=>navigate('/studentLogin')}style={{fontSize:'18px',marginRight:'20px'}}>Student</p>
-              <p onClick={()=>navigate('/adminLogin')} style={{fontSize:'18px',marginRight:'20px'}}>Admin</p>
+              <div className="headerNavbarContainer">
+              <p onClick={()=>navigate('/')} className='headerNavbarLink'>Home</p>
+              <p onClick={()=>navigate('/studentLogin')} className='headerNavbarLink'>Student</p>
+              <p onClick={()=>navigate('/adminLogin')} className='headerNavbarLink'>Admin</p>
               </div>
               </div>
         <div style={{display:'flex',justifyContent:'center',alignItems:'center',marginTop:'300px'}}>
