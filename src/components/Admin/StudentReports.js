e7f6264a1868f8e6392b96665d9c20b1be6ee757
sriram-sahu <sriramsahu510@gamail.com>
2023-06-23 09:21:22 +0530

    tables updated

--- src/components/Admin/StudentReports.js
+++ src/components/Admin/StudentReports.js
@@ -279,7 +279,7 @@ function StudentReports() {
                   columns={columns}
                   initialState={{
                     pagination: {
-                      paginationModel: { page: 0, pageSize: 5 },
+                      paginationModel: { page: 0, pageSize: 10 },
                     },
                   }}
                   pageSizeOptions={[5, 10, 15, 20]}
